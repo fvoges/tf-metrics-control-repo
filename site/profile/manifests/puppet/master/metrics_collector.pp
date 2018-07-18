a8ba635c68e820d2d77692317ae0834d625195c4
Federico Voges <federico@puppet.com>
2018-07-18 18:24:17 +0100

    add new metrics collector puppet profile

---
+++ site/profile/manifests/puppet/master/metrics_collector.pp
@@ -0,0 +1,12 @@
+class profile::puppet::master::metrics_collector {
+
+  $nodes_query = 'inventory[certname] {
+    trusted.extensions.pp_role = "puppet::master"
+    or
+    trusted.extensions.pp_role = "puppet::cm"
+  }'
+
+  class { '::puppet_metrics_collector':
+    puppetserver_hosts => puppetdb_query($nodes),
+  }
+}
