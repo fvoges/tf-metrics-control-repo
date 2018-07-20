2c2ad5d1954f620c38c4430673522c94157e5265
Federico Voges <federico@puppet.com>
2018-07-20 20:14:42 +0100

    add dashboard to mom's profile

--- site/role/manifests/puppet/master/mom.pp
+++ site/role/manifests/puppet/master/mom.pp
@@ -1,5 +1,6 @@
 class role::puppet::master::mom {
 
   include ::profile::puppet::master::autosign
-
+  include ::profile::puppet::master::metrics_collector
+  include ::profile::puppet::master::metrics_dashboard
 }
