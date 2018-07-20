bcbc97b85438f737152c1eb541926ee01ec3c0e4
Federico Voges <federico@puppet.com>
2018-07-20 20:14:12 +0100

    update puppetdb query to pull new trusted facts

--- site/profile/manifests/puppet/master/metrics_collector.pp
+++ site/profile/manifests/puppet/master/metrics_collector.pp
@@ -3,7 +3,9 @@ class profile::puppet::master::metrics_collector {
   $nodes_query = 'inventory[certname] {
     trusted.extensions.pp_role = "puppet::master"
     or
-    trusted.extensions.pp_role = "puppet::cm"
+    trusted.extensions.pp_role = "puppet::master::mom"
+    or
+    trusted.extensions.pp_role = "puppet::master::cm"
   }'
 
   $nodes = puppetdb_query($nodes_query).map |$value| { $value["certname"] }
