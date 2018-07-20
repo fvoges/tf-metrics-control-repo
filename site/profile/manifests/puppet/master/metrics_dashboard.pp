2836c4230eccdc8a5381318150eb97ffe59ca471
Federico Voges <federico@puppet.com>
2018-07-20 20:37:21 +0100

    update to query puppetdb for masters list

--- site/profile/manifests/puppet/master/metrics_dashboard.pp
+++ site/profile/manifests/puppet/master/metrics_dashboard.pp
@@ -1,5 +1,17 @@
 class profile::puppet::master::metrics_dashboard {
+  $nodes_query = 'inventory[certname] {
+    trusted.extensions.pp_role = "puppet::master"
+    or
+    trusted.extensions.pp_role = "puppet::master::mom"
+    or
+    trusted.extensions.pp_role = "puppet::master::cm"
+  }'
+
+  $nodes = puppetdb_query($nodes_query).map |$value| { $value["certname"] }
+
   class { '::puppet_metrics_dashboard':
+    use_dashboard_ssl      => true,
     add_dashboard_examples => true,
+    master_list            => $nodes,
   }
 }
